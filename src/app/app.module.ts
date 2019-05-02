cc5fa7f031b8be7c4f5fb03f391ae33a9ec4b64d
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2019-05-02 18:02:04 +0200

    initial setup, added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+// Material:
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    // Material:
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
